bb4acae5cdb13b11622826c97df16ebf3feff7e0
winnerkypt <kanyapat.winnerkypt@mail.kmutt.ac.th>
2022-08-21 13:34:51 +0700

    use role attribute class

--- OASIP-WEB/OASIP-BE/src/main/java/sit/oasip/dtos/UserDTO/AddUserDTO.java
+++ OASIP-WEB/OASIP-BE/src/main/java/sit/oasip/dtos/UserDTO/AddUserDTO.java
@@ -7,6 +7,7 @@ import org.hibernate.validator.constraints.Length;
 import org.springframework.util.ObjectUtils;
 
 
+import javax.validation.Constraint;
 import javax.validation.constraints.AssertTrue;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
@@ -15,7 +16,7 @@ import javax.validation.constraints.NotBlank;
 @Getter
 @Setter
 public class AddUserDTO {
-
+    
     @NotBlank(message = "please fill user name")
     @Length(min = 1, max = 100, message="size must be between 1 and 100")
     private String Name;

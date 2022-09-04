d233ffcdc2ec00a8b5db8c6baafbe5bcdfa08c43
salmon <iamfhaa@gmail.com>
2022-09-04 12:46:40 +0700

    Revert "Merge branch 'front-end-develop' into BACKEND"
    This reverts commit a1b5f66e239c0520be3a61e90548912b1c514728.

--- OASIP-WEB/OASIP-BE/src/main/java/sit/oasip/javainuse/config/WebSecurityConfig.java
+++ OASIP-WEB/OASIP-BE/src/main/java/sit/oasip/javainuse/config/WebSecurityConfig.java
@@ -56,6 +56,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 // dont authenticate this particular request
                 .authorizeRequests().antMatchers("/api/login","/api/users/signup" ).permitAll().
 
+
                 // all other requests need to be authenticated
                         anyRequest().authenticated().and().
                 // make sure we use stateless session; session won't be used to

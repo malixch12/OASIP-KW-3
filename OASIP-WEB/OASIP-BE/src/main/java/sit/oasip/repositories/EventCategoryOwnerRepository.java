a254116e6c902094a82330c589e8e663c1e88476
winnerkypt <winnerbest089@gmail.com>
2022-12-19 16:42:35 +0700

    fix

--- OASIP-WEB/OASIP-BE/src/main/java/sit/oasip/repositories/EventCategoryOwnerRepository.java
+++ OASIP-WEB/OASIP-BE/src/main/java/sit/oasip/repositories/EventCategoryOwnerRepository.java
@@ -8,7 +8,7 @@ import sit.oasip.entities.User;
 import java.util.List;
 import java.util.Optional;
 
-public interface EventCategoryOwnerRepository extends JpaRepository<Eventcategory,Integer> {
+public interface EventCategoryOwnerRepository extends JpaRepository<EventCategoryOwner,Integer> {
    EventCategoryOwner findById(int ownerId);
    List<EventCategoryOwner> findByEventCategoryID(Eventcategory cateId);
    List<EventCategoryOwner> findByUserID(int userId);
